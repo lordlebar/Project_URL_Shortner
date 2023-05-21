c3fbfabce14347fec5faca89754e61ff51b9c12b
Corentin Lebarilier <corentin.lebarilier@epita.fr>
2023-05-21 16:18:46 +0200

    Docker ++

--- src/db/config.php
+++ src/db/config.php
@@ -1,6 +1,6 @@
 <?php
-	define("DB_HOST", "localhost");
-	define("DB_USER", "root"); // quentin.guyot
-	define("DB_PASSWORD", ""); // guyotjul352
-	define("DB_BASE", "ACQTX");
+	define("DB_HOST", "db"); // localhost | db process.env.DB_HOST
+	define("DB_USER", "php_url_shortner"); // root | php_url_shortner process.env.DB_USER
+	define("DB_PASSWORD", "php_url_shortner"); // root | php_url_shortner  process.env.DB_PASSWORD
+	define("DB_BASE", "ACQTX"); // php_url_shortner
 ?>
