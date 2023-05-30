6f650fe9ffcbc93cb95e7704db32c6fc873cc0c9
Corentin Lebarilier <corentin.lebarilier@epita.fr>
2023-05-30 20:48:37 +0200

    FEAT: implement sentry

--- src/db/config.php
+++ src/db/config.php
@@ -1,4 +1,8 @@
 <?php
+require $_SERVER["DOCUMENT_ROOT"] . "/Project_URL_Shortner/vendor/autoload.php";
+
+\Sentry\init(['dsn' => 'https://62c9c1fe0ee34777b9c70b8e5f6e0fda@o4505257204842496.ingest.sentry.io/4505274352336896' ]);
+
 define("DB_HOST", "db"); // localhost | db
 define("DB_USER", "php_url_shortner"); // root | php_url_shortner
 define("DB_PASSWORD", "php_url_shortner"); // root | php_url_shortner
